62d8ef667a3546a61147a5907f30dc93fef32ac9
Juan Pablo Sanchez Perez <137138320+JuanSanchez1001@users.noreply.github.com>
2023-11-29 17:26:07 -0600

    Resolviendo conflictos branchB

--- myLibrary.py
+++ myLibrary.py
@@ -1,12 +1,7 @@
 import math
 
-<<<<<<< HEAD
 def getArea(side1, side2):
     return side1 * side2
-=======
-def getArea(sideA, sideB):
-    return sideA * sideB + 2
->>>>>>> branchB
 
 def getPermiter(sideB, sideA):
     return (sideB * sideA) * 2
