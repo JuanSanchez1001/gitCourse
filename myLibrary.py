45ef730cee9e6b3b3d105e0bff4d0f5a1dbdea41
Juan Pablo Sanchez Perez <137138320+JuanSanchez1001@users.noreply.github.com>
2023-11-29 17:10:41 -0600

    Cambios en getPerimeter

--- myLibrary.py
+++ myLibrary.py
@@ -3,7 +3,7 @@ import math
 def getArea(sideA, sideB):
     return sideA * sideB
 
-def getPermiter(sideA, sideB):
+def getPermiter(sideB, sideA):
     return (sideB * sideA) * 2
 
 def getAreaCircle(sideA, sideB):
