bbb61778bfc263dda0ea575f8233a8fe023c7522
Juan Pablo Sanchez Perez <137138320+JuanSanchez1001@users.noreply.github.com>
2023-11-29 17:15:27 -0600

    Cambios en branchA

--- myLibrary.py
+++ myLibrary.py
@@ -1,10 +1,11 @@
 import math
 
-def getArea(sideA, sideB):
-    return sideA * sideB
+def getArea(side1, side2):
+    return side1 * side2
 
 def getPermiter(sideB, sideA):
     return (sideB * sideA) * 2
 
 def getAreaCircle(sideA, sideB):
     return sideA + sideB
+
